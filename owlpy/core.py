828bc8a2bb2670c1af7f626d137103acc9fbb804
jbeleno <jsbeleno@gmail.com>
2016-11-25 15:18:54 -0200

    fix: STAMP working, but it seems the algorithm is slower than I though. As I though, the algorithm is sensitive to bad subsequence sizes

--- owlpy/core.py
+++ owlpy/core.py
@@ -7,27 +7,30 @@
         for Time Series: A Unifying View that Includes Motifs, Discords and 
         Shapelets. IEEE ICDM 2016.
 '''
+import numpy as np
+import pandas as pd
+import time
 
-import numpy
+import matplotlib.pyplot as plt
 
 def slidingDotProduct(Q, T):
     n = len(T)
     m = len(Q)
     
-    Ta = T
+    Ta = list(T) # Copy the T without modifying the original
     [Ta.append(0) for i in range(0, n)]
     
-    Qr = reversed(Q)
+    Qr = Q[::-1]
     
-    Qra = Qr
+    Qra = list(Qr) # Copy the T without modifying the original
     [Qra.append(0) for i in range(0, 2*n - m)]
     
-    Qraf = numpy.fft.fft(Qra)
-    Taf = numpy.fft.fft(Ta)
+    Qraf = np.fft.fft(Qra)
+    Taf = np.fft.fft(Ta)
     
-    QT = numpy.fft.ifft(numpy.multiply(Qraf, Taf))
+    QT = np.fft.ifft(np.multiply(Qraf, Taf))
     
-    return QT
+    return QT[m:n]
     
 def calculateDistanceProfile(Q, T, QT, mean_Q, std_Q, MT, ET):
     n = len(T)
@@ -39,7 +42,7 @@ def calculateDistanceProfile(Q, T, QT, mean_Q, std_Q, MT, ET):
         b = m*std_Q*ET[i]
         c = a/b
         
-        di = numpy.sqrt(2*m*(1 - c))
+        di = np.sqrt(2*m*(1 - c))
         D.append(di)
         
     return D
@@ -50,10 +53,10 @@ def computeMeanStd(Q, T):
     n = len(T)
     m = len(Q)
     
-    mean_Q = numpy.mean(Q)
-    std_Q = numpy.std(Q)
-    MT = [numpy.mean(T[range(i, i + m)]) for i in range(0, n - m)]
-    ET = [numpy.std(T[range(i, i + m)]) for i in range(0, n - m)]
+    mean_Q = np.mean(Q)
+    std_Q = np.std(Q)
+    MT = [np.mean(T[i : i + m]) for i in range(0, n - m)]
+    ET = [np.std(T[i : i + m]) for i in range(0, n - m)]
     
     return mean_Q, std_Q, MT, ET
     
@@ -66,23 +69,63 @@ def mass(Q, T):
     return D
     
 def elementWiseMin(Pab, Iab, D, idx):
+    Pab_copy = [1000000 for i in range(0, len(Pab))]
+    flag = False
+    
     for i in range(0, len(D)):
-        if(numpy.min(D[i], Pab[i]) == D[i]):
+        if(Pab[i] == float('Inf')):
+            Pab_copy[i] = D[i]
+            Iab[i] = idx
+            flag = True
+        elif(D[i] < Pab[i]):
             Pab[i] = D[i]
             Iab[i] = idx
             
-    return Pab, Iab
+    if flag:
+        return Pab_copy, Iab
+    else:
+        return Pab, Iab
     
     
 
 def stamp(Ta, Tb, m):
     nb = len(Tb)
-    Pab = [float('Inf') for i in range(0, nb - m)]
-    Iab = [0 for i in range(0, nb - m)]
+    na = len(Ta)
+    Pab = [float('Inf') for i in range(0, na - m)]
+    Iab = [0 for i in range(0, na - m)]
     idxes = range(0, nb - m)
     
     for idx in idxes:
-        D = mass(Tb[range(idx, idx + m)], Ta)
+        print(idx)
+        D = mass(Tb[idx : idx + m], Ta)
         Pab, Iab = elementWiseMin(Pab, Iab, D, idx)
         
     return Pab, Iab
+
+# Quick Test
+def test_stamp(Ta, Tb, m):
+    start_time = time.time()
+    
+    df_serie_a = pd.read_csv(Ta, header= 0)
+    df_serie_b = pd.read_csv(Tb, header= 0)
+    
+    ts_a = np.ravel(df_serie_a['value']).tolist()
+    ts_b = np.ravel(df_serie_b['value']).tolist()
+    
+    Pab, Iab = stamp(ts_a, ts_b, m)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    
+    plot_graphics(Pab)
+    
+def plot_graphics(values):
+    fig_width = 16
+    fig_height = 8
+    fig_dpi = 100
+    plt.figure(figsize=(fig_width, fig_height), dpi=fig_dpi)
+
+    plt.plot(range(0,len(values)), values, '#ff5722')
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.show()
+
+
